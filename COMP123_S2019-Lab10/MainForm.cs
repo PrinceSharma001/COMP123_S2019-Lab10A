74ba2c0929abed15b35b7f9e90ac5eac0c9d2d46
301043771 <301043771@PR-E216-66>
2019-07-18 14:56:54 -0400

    COMP123_S2019-Lab10A multiple forms

---
+++ COMP123_S2019-Lab10/MainForm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP123_S2019_Lab10
+{
+    public partial class MainForm : Form
+    {
+        
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            Program.Forms[FormName.START_FORM].Show();
+            this.Hide();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            Program.Forms[FormName.END_FORM].Show();
+            this.Hide();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            ImperialTableLayoutPanel.Visible = true;
+            MetricTableLayoutPanel.Visible = false;
+        }
+
+        private void radioButton1_CheckedChanged_1(object sender, EventArgs e)
+        {
+            MetricTableLayoutPanel.Visible = true;
+            ImperialTableLayoutPanel.Visible = false;
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- COMP123_S2019-Lab10/Program.cs
+++ COMP123_S2019-Lab10/Program.cs
@@ -6,8 +6,11 @@ using System.Windows.Forms;
 
 namespace COMP123_S2019_Lab10
 {
-    static class Program
+    public static class Program
     {
+
+        public static Dictionary<FormName, Form> Forms;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -16,7 +19,13 @@ namespace COMP123_S2019_Lab10
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            Forms = new Dictionary<FormName, Form>();
+            Forms.Add(FormName.START_FORM, new StartForm());
+            Forms.Add(FormName.MAIN_FORM, new MainForm());
+            Forms.Add(FormName.END_FORM, new EndForm());
+
+            Application.Run(Forms[FormName.START_FORM]);
         }
     }
 }
